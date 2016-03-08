dc5af56887b930c3bf7c55c111153f9e7a81e427
yu xiaodong <yuxiaodong@yus-MacBook-Air.local>
2016-03-09 07:41:15 +0800

    react-jyfs

---
+++ react-jyfs/webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  context: __dirname,
+  entry: __dirname + '/App/main.jsx',
+  output: {
+    path: __dirname + '/build',
+    filename: "bundle.js",
+    publicPath: '/',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+  
+}
