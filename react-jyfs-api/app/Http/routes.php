9494212aa8adafe0fcdcbc4bf8ae616c1b40a3cd
bdtgzj <bdtgzj@gmial.com>
2016-03-15 17:13:16 +0800

    add react-jyfs-api

---
+++ react-jyfs-api/app/Http/routes.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Routes File
+|--------------------------------------------------------------------------
+|
+| Here is where you will register all of the routes in an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/welcome', function () {
+  return view('welcome');
+});
+
+Route::get('/', function () {
+  $data = array(
+    array('url'=>'http://google.com', 'title'=>'google'),
+    array('url'=>'http://baidu.com', 'title'=>'baidu')
+  );
+  return json_encode($data);
+});
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => ['web']], function () {
+    //
+});
--- react-jyfs/App/main.jsx
+++ react-jyfs/App/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
 ReactDOM.render(
-  <App url={'http://localhost'} />,
+  <App url={'http://cmsapi.jyfs.com'} />,
   document.body.appendChild(document.createElement('div'))
 );
 
