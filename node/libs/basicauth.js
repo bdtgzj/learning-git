8326b1407e57400ebf65974f77a94b294c7da347
yu xiaodong <yuxiaodong@yus-MacBook-Air.local>
2016-06-13 23:15:47 +0800

    node

--- node/libs/basicauth.js
+++ node/libs/basicauth.js
@@ -1,6 +1,7 @@
 var config = require('../config');
 var auth = require('basic-auth');
 var User = require('../proxy').User;
+var hashcrypt = require('./hashcrypt');
 
 /**
  * api access authentication according basic authentication.
@@ -9,16 +10,17 @@ var basicAuth = function(req, res, next) {
   var credentials = auth(req);
   if (!credentials) {
     res.set('WWW-Authenticate', 'Basic realm="ehomeguru.com.cn"');
-    return res.status(401).end('Access denied');
+    return res.status(401).end('Access denied.');
   }
-  User.getUserByNameEmailMPhonePass(credentials.name, credentials.pass, function(err, users) {
+  User.getUserByNameEmailMPhonePass(credentials.name, hashcrypt.sha1(credentials.pass), function(err, users) {
     if (err) {
       next(err);
     } else {
+      console.log(credentials.user + credentials.pass + users + users[0]);
       var user = users.length > 0 ? users[0] : null;
       if (!user) {
         res.set('WWW-Authenticate', 'Basic realm="ehomeguru.com.cn"');
-        res.status(401).end('Access denied');
+        res.status(401).end('Access denied.');
         //res.json({desc: '', valid: false});
       } else {
         next();
