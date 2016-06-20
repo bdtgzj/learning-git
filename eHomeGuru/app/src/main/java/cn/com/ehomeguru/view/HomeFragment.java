15d2fa2817d9aec999534949f40256afa4406d42
xiaodong yu <xiaodongyu@chen-lins-MacBook-Pro.local>
2016-06-20 17:02:30 +0800

    ehg

--- eHomeGuru/app/src/main/java/cn/com/ehomeguru/view/HomeFragment.java
+++ eHomeGuru/app/src/main/java/cn/com/ehomeguru/view/HomeFragment.java
@@ -1,10 +1,10 @@
 package cn.com.ehomeguru.view;
 
-import android.app.Fragment;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
--- eHomeGuru/app/src/main/java/cn/com/ehomeguru/view/MainActivity.java
+++ eHomeGuru/app/src/main/java/cn/com/ehomeguru/view/MainActivity.java
@@ -1,8 +1,8 @@
 package cn.com.ehomeguru.view;
 
 import android.support.annotation.IdRes;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnMenuTabClickListener;
@@ -17,6 +17,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Add Default Fragment
+        if (findViewById(R.id.fragment_container) != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity framelayout layout
+            HomeFragment homeFragment = new HomeFragment();
+            homeFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, homeFragment, getResources().getString(R.string.fragment_home))
+                    .commit();
+        }
 
         // BottomBar init
         bottomBar = BottomBar.attach(this, savedInstanceState);
