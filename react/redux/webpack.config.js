2b5c9bbba63fd62a316238a0c528b3bac722f68b
xiaodong yu <xiaodongyu@chen-lins-MacBook-Pro.local>
2016-07-21 17:05:15 +0800

    ehg

---
+++ react/redux/webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var TransferWebpackPlugin = require('transfer-webpack-plugin');
+
+module.exports = {
+  context: __dirname,
+  entry: {
+    test_redux: './test_redux.js',
+    index: './index.js'
+  },
+  output: {
+    path: path.join(__dirname, 'build'),
+    publicPath: '/',
+    filename: "[name].js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new TransferWebpackPlugin([
+      { from: 'www'}
+    ], path.join(__dirname, 'src'))
+  ]
+
+}
