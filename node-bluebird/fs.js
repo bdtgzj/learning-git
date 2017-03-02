a8514c9fd00640bb35b89970c7cd6f61d1542c12
xiaodong yu <xiaodongyu@chen-lins-MacBook-Pro.local>
2017-03-02 17:53:49 +0800

    :)

--- node-bluebird/fs.js
+++ node-bluebird/fs.js
@@ -16,13 +16,16 @@ Promise.config({cancellation: true});
 var FS = Promise.promisifyAll(require('./proxy/fs'));
 
 p = FS.getFileContentAsync('/Users/xiaodongyu/Downloads/url.html')
-  .then((data) => {
-    console.log(data.toString());
-    p.cancel();
-    return '1';
-  })
-  .then((data) => {
-    console.log(data);
+  .then((data1) => {
+    console.log(data1.toString());
+    // p.cancel();
+    // console.log("continue?");
+    return FS.getFileContentAsync('/Users/xiaodongyu/Downloads/url.html');
+    //return '1';
+  })  
+  .then((data2) => {
+    //console.log(data1);
+    console.log(data2.toString());
     require('./proxy/fs').getFileContent('/Users/xiaodongyu/Downloads/url.html', function(err, data) {
       if (err) {
         console.log(err);
