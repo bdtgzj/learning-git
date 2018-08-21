61d19db5535e047a0977100c0d8e564b94491691
xiaodong yu <xiaodongyu@192.168.8.110>
2018-08-21 17:56:46 +0800

    add fibos

---
+++ fibos/fibos_hello/fibos_node/node.js
@@ -0,0 +1,19 @@
+var fibos = require('fibos');
+
+fibos.load("http", {
+  "http-server-address": "0.0.0.0:8888" // default 127.0.0.1:8888
+});
+fibos.load("chain", {
+  "delete-all-blocks": true
+});
+fibos.load("net", {
+  "p2p-listen-endpoint": "0.0.0.0:9876" // default 0.0.0.0:9876
+});
+fibos.load("chain_api");
+fibos.load("history_api");
+fibos.load("producer", {
+  'producer-name': 'eosio',
+  'enable-stale-production': true
+});
+
+fibos.start();
