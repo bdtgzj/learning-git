a6146c0dc2aae29d5a9ffd0520dc40512abd9ec1
yu xiaodong <yuxiaodong@yus-MacBook-Air.local>
2018-08-23 07:38:39 +0800

    add eos

--- fibos/fibos_hello/fibos_client/call_contract.js
+++ fibos/fibos_hello/fibos_client/call_contract.js
@@ -1,28 +1,16 @@
-var FIBOS = require('fibos.js')
-var fs = require("fs");
+var FIBOS = require('./initClient.js');
+
 var config = {
-  "chainId": "cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f",
-  "producer-name": "eosio",
-  "public-key": "EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV",
+  "public-key": "FO6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV",
   "private-key": "5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3",
-  "httpEndpoint": "http://127.0.0.1:8888",
+  "contractName": "hello"
 };
 
 // new FIBOS client
-var fibos = FIBOS({
-  chainId: config["chainId"],
-  keyProvider: config["private-key"],
-  httpEndpoint: config["httpEndpoint"],
-  logger: {
-    log: null,
-    error: null
-  }
-});
-
-var contractName = "hello";
+var fibos = FIBOS(config["private-key"]);
 
 //call abi
-var ctx = fibos.contractSync(contractName);
-ctx.hiSync('hello', {
-    authorization: contractName
+var ctx = fibos.contractSync(config['contractName']);
+let i = ctx.hiSync('hello', {
+    authorization: config['contractName']
 });
--- fibos/fibos_hello/fibos_client/deploy_contract.js
+++ fibos/fibos_hello/fibos_client/deploy_contract.js
@@ -1,44 +1,30 @@
-var FIBOS = require('fibos.js')
+var FIBOS = require('./initClient.js')
 var fs = require("fs");
 var config = {
-  "chainId": "cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f",
-  "producer-name": "eosio",
-  "public-key": "EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV",
-  "private-key": "5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3",
-  "httpEndpoint": "http://127.0.0.1:8888",
+    "public-key": "FO6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV",
+    "private-key": "5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3",
+    "contractName": "hello"
 };
 
 // new FIBOS client
-var fibos = FIBOS({
-  chainId: config["chainId"],
-  keyProvider: config["private-key"],
-  httpEndpoint: config["httpEndpoint"],
-  logger: {
-    log: null,
-    error: null
-  }
-});
-
-var contractName = "hello";
+var fibos = FIBOS(config["private-key"]);
 
 //新建 hello 账户
 fibos.newaccountSync({
   creator: 'eosio',
-  name: "hello",
+  name: "hello1",
   owner: config["public-key"],
   active: config["public-key"]
 });
 
-
 //setcode
-var js_code = fs.readTextFile("./contracts/hello.js");
-fibos.setcodeSync(contractName, 0, 0, fibos.compileCode(js_code));
+var js_code = fs.readTextFile("./fibos_client/contracts/hello.js");
+fibos.setcodeSync(config["contractName"], 0, 0, fibos.compileCode(js_code));
 
 //getcode
-var code = fibos.getCodeSync(contractName, true);
-
+var code = fibos.getCodeSync(config["contractName"], true);
 console.log("code:", code);
 
 //setabi
-var abi = JSON.parse(fs.readTextFile("./contracts/hello.abi"));
-fibos.setabiSync(contractName, abi);
+var abi = JSON.parse(fs.readTextFile("./fibos_client/contracts/hello.abi"));
+fibos.setabiSync(config["contractName"], abi);
