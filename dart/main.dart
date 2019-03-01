0c0b6c81bf00ceacdcd36d0aaa0ac0fc6e55b574
xiaodong yu <xiaodongyu@chen-lins-MacBook-Pro.local>
2019-03-01 17:19:08 +0800

    m dart

--- dart/main.dart
+++ dart/main.dart
@@ -3,6 +3,7 @@ import 'package:meta/meta.dart';
 void main() {
   funcName(p1: 'p1');
   funcName1('p2', 'p3');
+  var p = ParentClass(11);
 }
 
 void funcName({@required String p1, String p2}) {
@@ -14,13 +15,28 @@ void funcName1(String p1, String p2, [String p3, int p4]) {
 }
 
 void obj() {
+  var names = List<String>();
+names.addAll(['Seth', 'Kathy', 'Lars']);
+names.add(42); // Error
 
 }
 
-class HelloClass {
+
+
+class ParentClass {
   num x = 0;
   // Syntactic sugar: HelloClass(this.x)
-  HelloClass(num x) {
-    this.x = x;
+  ParentClass(num x) :x = x {
+    // this.x = x;
+    print(this.x);
   }
+  //
+  //
+  ParentClass.fromJson(Map<String, num> json) {
+
+  }
+}
+
+class ChildClass {
+
 }
