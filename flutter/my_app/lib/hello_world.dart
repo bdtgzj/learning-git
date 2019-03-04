4846dfc69fd351c55e384ceaa997ec6e4badf083
xiaodong yu <xiaodongyu@chen-lins-MacBook-Pro.local>
2019-03-04 17:17:39 +0800

    m flutter

---
+++ flutter/my_app/lib/hello_world.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(
+    Center(
+      child: Text(
+        'Hello, World!',
+        textDirection: TextDirection.ltr,
+      ),
+    )
+  );
+}
