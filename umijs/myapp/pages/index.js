fe4a078d890652ed0ae4ced7b44c7658379cfa43
yu xiaodong <yuxiaodong@yus-MacBook-Air.local>
2019-10-29 07:17:45 +0800

    modify umi

--- umijs/myapp/pages/index.js
+++ umijs/myapp/pages/index.js
@@ -1,10 +1,12 @@
 
 import styles from './index.css';
+import Link from 'umi/link';
 
 export default function() {
   return (
     <div className={styles.normal}>
       <h1>Page index</h1>
+      <Link to="/users">go to /users</Link>
     </div>
   );
 }
---
+++ umijs/myapp/pages/users.js
@@ -0,0 +1,12 @@
+
+import styles from './users.css';
+import router from 'umi/router';
+
+export default function() {
+  return (
+    <div className={styles.normal}>
+      <h1>Page users</h1>
+      <button onClick={() => { router.goBack(); }}>go back</button>
+    </div>
+  );
+}
