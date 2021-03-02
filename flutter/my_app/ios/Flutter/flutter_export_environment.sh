eb0bb48f2d7de684fd413dcf67f45193633af51c
yu xiaodong <bdtgzj@gmail.com>
2021-03-02 08:48:24 +0800

    d

---
+++ flutter/my_app/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/usr/local/flutter"
+export "FLUTTER_APPLICATION_PATH=/Volumes/TF64/Learning/learning-git/flutter/my_app"
+export "FLUTTER_TARGET=/Volumes/TF64/Learning/learning-git/flutter/my_app/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/usr/local/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
